843d266c7c5ba5e6b4ef5f6e11873ca31c45451f
Ian <ianjazz246@gmail.com>
2020-11-23 20:15:38 -0800

    User controlled network stepping

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # random-boolean-network
+A program to create, run, load, and save boolean networks.
+Writen in C++17 (Or at least C++11. I don't know if I even use any C++17 features.)
+Inspired by the Computerphile video
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "network.h"
 #include <string>
 #include <iostream>
+#include <exception>
 
 const std::string defaultNetworkPath("networks/basic.txt");
 
@@ -8,12 +9,33 @@ int main(int argc, char** argv) {
 	Network net(" ", "0");
 	//net.randomizeNetwork(10, 2, 2);
 
-	net.loadFromFile(defaultNetworkPath);
-
+	try {
+		std::cout << "Loading network state from file" << argv[1] << '\n';
+		if (argc > 1) {
+			std::cout << argv[1] << '\n';
+			net.loadFromFile(argv[1]);
+		} else {
+			net.randomizeNetwork(10, 1, 1);
+		}
+	} catch(const std::runtime_error& e) {
+		std::cerr << e.what();
+		return 1;
+	}
 	std::cout << net.exportState();
 
-	std::cout << net;
+	char inChar;
 
+	std::cout << "Hit enter to step through network. Enter q to quit\n";
+
+	while (true) {
+		std::cin.get(inChar);
+		if (inChar != 'q') {
+			net.step();
+			std::cout << net;
+		} else {
+			return 0;
+		}
+	}
 
 	return 0;
 }
